2b4fd6be59906a5098b5f2cc56d492ecdfe7ef9c
foysal432 <foysalarham69@gmail.com>
2023-10-20 21:56:46 +0600

    added delate methood

--- index.js
+++ index.js
@@ -71,8 +71,13 @@ app.get('/addproduct',async(req,res)=>{
 })
 
 
-
-
+// delate operation
+app.delete('/addproduct/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query ={_id:new ObjectId(id)}
+  const result =await addCardCollection.deleteOne(query)
+  res.send(result)
+})
 
 // get all brand item
 app.get('/items/:brand',async(req,res)=>{
