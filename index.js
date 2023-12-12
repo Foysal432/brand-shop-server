7334bd53da6f09f81003dc345727160d970d3e7e
foysal432 <foysalarham69@gmail.com>
2023-12-12 21:29:04 +0600

    client repo change

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const brandCollection =client.db('brandDB').collection('brands')
     const alliteamCollection=client.db('brandDB').collection('items')
     const addCardCollection=client.db('brandDB').collection('addproduct')
